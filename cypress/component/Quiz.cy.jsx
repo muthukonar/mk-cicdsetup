0dceedb62becfbf9e4810c7fd8bd42a965aa0833
pechikonar <muthukonaruot@gmail.com>
2025-03-09 15:17:10 -0400

    Test2

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -15,7 +15,7 @@ describe('Quiz Component', () => {
 
   it('should start the quiz and display the first question', () => {
     cy.mount(<Quiz />);
-    cy.get('mutton').contains('Start Quiz').click();
+    cy.get('button').contains('Start Quiz').click();
     cy.get('.card').should('be.visible');
     cy.get('h2').should('not.be.empty');
   });
